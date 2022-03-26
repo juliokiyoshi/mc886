b1f6a11a2ad440c2436dfea6deca3d317da7babb
lucca paiva <luccafpaiva@gmail.com>
2022-03-26 13:26:04 -0300

    classe de linhas, e criacao das arestas dos poligonoes

--- lab01/Point.py
+++ lab01/Point.py
@@ -12,7 +12,7 @@ class Point:
     def printPoint(self):
         print(self.name + ":", self.coord)
 
-    def DistanceToPoint(self, point):
+    def DistanceToPoint(self, point) -> int:
         dst = math.sqrt((self.coord[0] - point.coord[0]) * (self.coord[0] - point.coord[0])
                         + (self.coord[1] - point.coord[1])*(self.coord[1] - point.coord[1]))
         return dst
--- lab01/Poligon.py
+++ lab01/Poligon.py
@@ -1,4 +1,5 @@
 import Point
+import Line
 
 
 class Poligon:
@@ -6,13 +7,20 @@ class Poligon:
         self.id = id
         self.points = []
         self.numberOfVertices = 0
+        self.edges = []
 
     def AddPoint(self, newPoint):
         self.points.append(newPoint)
         self.numberOfVertices += 1
 
+    def CreateEdge(self, point1, point2):
+        edge = Line.Line(point1, point2)
+        self.edges.append(edge)
+
     def printPoints(self):
         print("Poligono: ", self.id)
         for point in self.points:
             point.printPoint()
+        for edge in self.edges:
+            edge.PrintLine()
         print()
